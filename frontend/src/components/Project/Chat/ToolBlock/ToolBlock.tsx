77f021b24b5ddd776c6d7852501fa8152c0b449c
DESKTOP-TEN4NAB\user <ran.fridman.06@gmail.com>
2026-05-31 23:20:39 +0300

    feat: create ToolCallBlock component for rendering collapsible tool invocation details

--- frontend/src/components/Project/Chat/ToolBlock/ToolBlock.tsx
+++ frontend/src/components/Project/Chat/ToolBlock/ToolBlock.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Card, Text, Code, Group, Collapse, ActionIcon, ThemeIcon, Box, Badge, Tooltip, Divider } from '@mantine/core';
+import { Card, Text, Code, Group, Collapse, ActionIcon, ThemeIcon, Box, Badge, Tooltip, Divider, Accordion } from '@mantine/core';
 import {
   IconChevronDown,
   IconChevronUp,
@@ -11,9 +11,12 @@ import {
   IconCpu,
   IconHammer,
   IconCheck,
-  IconLoader
+  IconLoader,
+  IconTool,
+  IconChevronRight
 } from '@tabler/icons-react';
 import './ToolBlock.css';
+import { useDisclosure } from '@mantine/hooks';
 
 // Dynamically select an icon based on the tool's name
 export const getToolIcon = (name: string) => {
@@ -42,12 +45,28 @@ interface ToolCallBlockProps {
 }
 
 export const ToolCallBlock: React.FC<ToolCallBlockProps> = ({ name, children }) => {
-
-
+  const [open, { toggle }] = useDisclosure(false)
   const ToolIcon = getToolIcon(name ?? "");
 
   return (<>
-    {JSON.stringify(children)}  <Divider label={name}></Divider>
+    <Group w="100%" wrap="nowrap" gap="5" justify='space-between'>
+      <Group w="100%" wrap="nowrap" gap="5">
+
+        {/* <Text size="xs">{name}</Text> */}
+        <ThemeIcon variant='outline' color="dimmed" c="dimmed" size="xs" radius={5} >
+          <IconTool size={12} />
+        </ThemeIcon>
+
+        <Divider w="100%" label={name} labelPosition='left' />
+      </Group>
+      <ThemeIcon variant='transparent' onClick={toggle} size="xs" radius={5} >
+        <IconChevronRight size={16} color="var(--mantine-color-default-border)" />
+      </ThemeIcon>
+    </Group>
+    <Collapse expanded={open}>
+      {JSON.stringify(children)}
+    </Collapse>
+
   </>
   );
 };
